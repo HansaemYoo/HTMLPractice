b8b356a5ac200e3d4d47bcf198b8cd68f30bdaee
HansaemYoo <77716290+HansaemYoo@users.noreply.github.com>
2021-03-15 12:07:06 +0900

    python
    연습

---
+++ practice.py
@@ -0,0 +1,30 @@
+#숫자
+print(5)
+print(-10)
+print(3.14)
+print(1000)
+print(5+1)
+print(2*5)
+print(3*(3+1))
+
+#문자열
+print("풍선")
+print('풍선')
+print("ㅋ"*7)
+
+# 참/거짓
+print(6>10)
+print(True)
+print(not True)
+print(not(5>6))
+
+#변수
+print("고양이 이름은 연탄입니다.")
+print("연탄이는 4살이며, 산책을 좋아합니다.")
+
+animal = "고양이"
+name = "연탄"
+age = 4
+hobby = "산책"
+print(animal + " 이름은 " + name + "입니다.")
+print(name + "이는" + str(age) + "살이며, " + hobby + "을 좋아합니다.")
