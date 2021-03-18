081c3c7f10b5e6842dd6d1c71b723f9b0d3fee08
HansaemYoo <77716290+HansaemYoo@users.noreply.github.com>
2021-03-18 18:21:30 +0900

    python list
    파이썬 리스트 연습

--- 001.py
+++ 001.py
@@ -1,6 +1,10 @@
-#리스트
+#리스트(배열) []
 
 #지하철 칸별로 10명, 20명, 30명
+subway1 = 10
+subway2 = 20
+subway3 = 30
+# 위가 기존의 방식 이었다면 이젠 하나의 리스트에 넣기
 subway = [10, 20, 30]
 print(subway)
 
@@ -19,7 +23,7 @@ subway.insert(1, "정형돈")
 print(subway)
 
 #지하철에 있는 사람 뒤에서 한명 씩 내림
-print(subway.pop())
+print(subway.pop(2))
 print(subway)
 
 #같은 이름의 사람이 몇 명 있는 지 확인
@@ -30,4 +34,21 @@ print(subway.count("유재석"))
 #정렬
 num = [5,2,4,3,1]
 num.sort()
+print(num)
+
+# 순서 반대로
+num.reverse()
+print(num)
+
+# 모두 지우기
+num.clear()
+print(num)
+
+# 다양한 자료형 함께 사용
+list = ["치킨", 2, True]
+print(list)
+
+# 리스트 합치기
+num = [5,2,4,3,1]
+num.extend(list)
 print(num)
